c875541a22695433f5b6d25cc3d8075eb691f266
Esraa Ahmed <99386832+EsraaAbdelrazek@users.noreply.github.com>
2025-08-27 09:51:04 +0300

    Time: 959 ms (6.54%), Space: 0B (100%) - LeetHub

--- 1179-game-play-analysis-i/1179-game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/1179-game-play-analysis-i.sql
@@ -1,12 +1,18 @@
-# Write your MySQL query statement below
+-- # Write your MySQL query statement below
+
+-- SELECT 
+--     player_id  , 
+--     event_date AS first_login 
+-- FROM  Activity a1
+-- WHERE event_date = 
+--     (
+--         SELECT MIN(event_date) 
+--      FROM Activity a2 
+--      WHERE a1.player_id = a2.player_id
+--      )
 
 SELECT 
-    player_id  , 
-    event_date AS first_login 
-FROM  Activity a1
-WHERE event_date = 
-    (
-        SELECT MIN(event_date) 
-     FROM Activity a2 
-     WHERE a1.player_id = a2.player_id
-     )
+    player_id, 
+    MIN(event_date) AS first_login
+FROM Activity
+GROUP BY player_id;
